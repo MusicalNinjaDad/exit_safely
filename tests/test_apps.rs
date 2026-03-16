05d4f183c0621649ebca7b11dd806679f993503c
Mike Foster <i.am.mike.foster@gmail.com>
2026-03-16 12:05:56 +0000

    build once for integration tests

--- tests/test_apps.rs
+++ tests/test_apps.rs
@@ -2,23 +2,27 @@
 #![feature(never_type)]
 #![feature(try_trait_v2)]
 
-use std::{assert_matches::assert_matches, process::Command};
+use std::{assert_matches::assert_matches, path::PathBuf, process::Command};
 
 use escargot::CargoBuild;
 use exit_safely::Termination;
 use try_v2::Try;
 
-#[test]
-fn exit_ok() {
-    let app = CargoBuild::new()
+use std::sync::LazyLock;
+static APP: LazyLock<PathBuf> = LazyLock::new( ||
+    CargoBuild::new()
         .bin("fixture_app")
         .manifest_path("./tests/fixture_app/Cargo.toml")
         .current_release()
         .run()
         .unwrap()
         .path()
-        .to_owned();
-    let mut cmd = Command::new(&app);
+        .to_owned(),
+);
+
+#[test]
+fn exit_ok() {
+    let mut cmd = Command::new(APP.as_os_str());
     let output = cmd.output().unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
     assert_eq!("Hello, world!\n", stdout);
