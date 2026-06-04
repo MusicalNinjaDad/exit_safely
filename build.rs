52940bcd9134c62f424f191afd0e1dd8bc0c95bf
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-04 16:24:19 +0000

    ninja_build_rs

--- build.rs
+++ build.rs
@@ -1,22 +1,8 @@
-use autocfg::AutoCfg;
+use ninja_build_rs::{Result, nightly::Nightly};
 
-fn main() {
+fn main() -> Result<()> {
     let ac = autocfg::new();
-    stable_feature(&ac, "let_chains");
-    stable_feature(&ac, "if_let_guard");
-}
-
-fn stable_feature(ac: &AutoCfg, feature: &'static str) {
-    let cfg = format!("stable_{feature}");
-    let code = format!(
-        r#"
-    #![deny(stable_features)]
-    #![feature({feature})]
-    "#
-    );
-
-    autocfg::emit_possibility(&cfg);
-    if ac.probe_raw(&code).is_err() {
-        autocfg::emit(&cfg);
-    }
+    ac.emit_unstable_feature("let_chains");
+    ac.emit_unstable_feature("if_let_guard");
+    Ok(())
 }
