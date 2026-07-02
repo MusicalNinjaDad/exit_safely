3157b822fcc92f994252d5ce9344f7b93b588e32
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-02 12:06:16 +0000

    fmt

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
-use ninja_build_rs::{Result, nightly::{Nightly, cargo_allowed_features}};
+use ninja_build_rs::{
+    Result,
+    nightly::{Nightly, cargo_allowed_features},
+};
 
 fn main() -> Result<()> {
     let mut ac = autocfg::new();
     ac.set_edition(Some("2024".to_string()));
 
-    let allowed_features= cargo_allowed_features()?;
+    let allowed_features = cargo_allowed_features()?;
     ac.emit_unstable_feature("proc_macro_diagnostic", &allowed_features);
     ac.emit_unstable_feature("never_type", &allowed_features);
     ac.emit_unstable_feature("try_trait_v2", &allowed_features);
